0f67bdfa8a05c28e2a8e2a092e781d3d611322d4
Enrique Rodriguez <emrodriguez3e@gmail.com>
2022-09-16 14:11:54 -0700

    Create selenium_test.py
    upload initial code

---
+++ selenium_test.py
@@ -0,0 +1,35 @@
+import time
+from selenium import webdriver
+from selenium.webdriver.common.by import By
+
+from selenium.webdriver.common.action_chains import ActionChains
+from selenium.webdriver.common.keys import Keys
+from selenium.webdriver.chrome.service import Service
+
+
+
+
+
+ser = Service(executable_path="C:/Users/erodriguez/Documents/chromedriver.exe")
+op = webdriver.ChromeOptions()
+op.add_experimental_option('excludeSwitches', ['enable-logging'])
+driver = webdriver.Chrome(service=ser, options=op)
+
+
+def google_search(search_input):
+    driver.get('http://www.google.com')
+    driver.implicitly_wait(2)
+    text_box=driver.find_element(By.TAG_NAME, 'input')
+    text_box.send_keys(search_input+Keys.ENTER)
+
+    time.sleep(2)
+
+    clickable = driver.find_element(By.ID, 'footcnt')
+    ActionChains(driver).move_to_element(clickable).perform()
+
+
+google_search('blue')
+google_search('red')
+google_search('yellow')
+
+driver.quit()
