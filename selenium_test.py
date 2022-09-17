cb9762bea249f28d44412126c27c344beccecb85
Enrique Rodriguez <emrodriguez3e@gmail.com>
2022-09-17 13:53:04 -0700

    -Update file

--- selenium_test.py
+++ selenium_test.py
@@ -1,4 +1,4 @@
-import time
+import sys
 from selenium import webdriver
 from selenium.webdriver.common.by import By
 from selenium.webdriver.common.actions.wheel_input import ScrollOrigin
@@ -8,32 +8,44 @@ from selenium.webdriver.common.keys import Keys
 from selenium.webdriver.chrome.service import Service
 
 
-ser = Service(executable_path="C:/Users/erodriguez/Documents/chromedriver.exe")
+# Check system platform
+if sys.platform == 'darwin':
+    ser = Service(executable_path='/Users/enriquerodriguez/chromedriver')
+elif sys.platform == 'win32':
+    ser = Service(executable_path='C:/Users/erodriguez/Documents/chromedriver.exe')
+else:
+    print('Unable to find chrome driver')
+    exit()
+
 op = webdriver.ChromeOptions()
 op.add_experimental_option('excludeSwitches', ['enable-logging'])
-driver = webdriver.Chrome(service=ser, options=op)
+
+
+def safari_search(search_input):
+    driver = webdriver.Safari()
+    google_move(search_input, driver)
 
 
 def google_search(search_input):
-    driver.get('http://www.google.com')
+    driver = webdriver.Chrome(service=ser, options=op)
+    google_move(search_input, driver)
+
+
+def google_move(search_input, driver):
+    driver.get('https://www.google.com')
     driver.implicitly_wait(2)
-    text_box=driver.find_element(By.TAG_NAME, 'input')
-    text_box.send_keys(search_input+Keys.ENTER)
+    text_box = driver.find_element(By.TAG_NAME, 'input')
+    text_box.send_keys(search_input + Keys.ENTER)
 
-    time.sleep(2)
-    
-    #clickable = driver.find_element(By.ID, 'footcnt')
-    #ActionChains(driver).move_to_element(clickable).perform()
+    driver.implicitly_wait(5)
 
-    text_box=driver.find_element(By.TAG_NAME, 'input')
+    text_box = driver.find_element(By.TAG_NAME, 'input')
     scroll_origin = ScrollOrigin.from_element(text_box)
-    ActionChains(driver)\
-        .scroll_from_origin(scroll_origin, 0, 200)\
+    ActionChains(driver) \
+        .scroll_from_origin(scroll_origin, 0, 200) \
         .perform()
+    driver.quit()
 
 
-google_search('blue')
-google_search('red')
-google_search('yellow')
-
-driver.quit()
+safari_search('red')
+safari_search('blue')
